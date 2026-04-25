c10df0208b03ebd3459cd9d6368c1c0fc5785db5
Flav <flavien.valea@epfl.ch>
2026-04-25 16:31:06 +0200

    added cors header

--- api_v1/src/main.ts
+++ api_v1/src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { mkdirSync } from 'fs';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -28,6 +29,14 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
   app.set('trust proxy', 1); // trust first proxy hop
+
+  app.use(
+    cors({
+      origin: ['http://localhost:5173', 'https://api.campusmotion.ch'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
